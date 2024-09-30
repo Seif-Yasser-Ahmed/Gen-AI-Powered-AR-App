0afa2fb75bb40b6128a0dd1611d24c5f0ddac5dd
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-10-01 00:01:18 +0300

    added plugins needed

--- Flutter App/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter App/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
