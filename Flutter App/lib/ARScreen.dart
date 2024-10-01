e2283e5a71fe63cf7e1b1c507cc02bd526f27754
Abdallah El-Sakka <37080741+al-sakka@users.noreply.github.com>
2024-10-01 03:04:24 +0200

    added arcore plugin

--- Flutter App/lib/ARScreen.dart
+++ Flutter App/lib/ARScreen.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ARScreen extends StatefulWidget {
   const ARScreen({super.key});
@@ -8,27 +10,49 @@ class ARScreen extends StatefulWidget {
   State<ARScreen> createState() => _ARScreenState();
 }
 
-class _ARScreenState extends State<ARScreen>
-{
+class _ARScreenState extends State<ARScreen> {
+  late ArCoreController arCoreController;
+
   @override
-  Widget build(BuildContext context)
-  {
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF121212),
       appBar: AppBar(
         backgroundColor: const Color(0xFF121212),
         leading: IconButton(
-          onPressed: ()
-          {
+          onPressed: () {
             Navigator.pop(context);
           },
           icon: const Icon(Icons.arrow_back_ios_rounded),
-          color: Colors.white, // Set icon color if necessary
+          color: Colors.white,
         ),
       ),
-      body: Column(
-        children: [],
+      body: ArCoreView(
+        onArCoreViewCreated: _onArCoreViewCreated,
       ),
     );
+  }
+
+  void _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+    _addImageNode();
+  }
+
+  Future<void> _addImageNode() async {
+    final ByteData imageBytes = await rootBundle.load('assets/test.png');
+    final Uint8List imageData = imageBytes.buffer.asUint8List();
+
+    final imageNode = ArCoreNode(
+      image: ArCoreImage(bytes: imageData),
+      position: vector.Vector3(0, 0, -1.5), // Position 1.5 meters away
+    );
+
+    arCoreController.addArCoreNode(imageNode);
   }
 }
